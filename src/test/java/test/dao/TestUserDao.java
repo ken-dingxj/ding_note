23f8a7fc5c798e8ed869eb194aec175c4020343e
dingxuejin <dingxuejin@cpsol.cn>
2019-05-12 17:08:41 +0800

    factor:项目编写mybatic测试类

--- src/test/java/test/dao/TestUserDao.java
+++ src/test/java/test/dao/TestUserDao.java
@@ -4,8 +4,8 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.lcz.cloud_note.dao.UserDao;
-import com.lcz.cloud_note.entity.User;
+import com.dingjin.ding_note.dao.UserDao;
+import com.dingjin.ding_note.entity.User;
 
 
 public class TestUserDao {
@@ -15,20 +15,6 @@ public class TestUserDao {
 		ApplicationContext ac=new ClassPathXmlApplicationContext("conf/spring_mybatis.xml");
 		UserDao dao = ac.getBean("userDao",UserDao.class);
 		User user = dao.findByName("demo");
-		System.out.println(user);
-	}
-	//测试注册账号
-	@Test
-	public void testSave() {
-		String[] conf= {"conf/spring_mybatis.xml","conf/spring_mvc.xml"};
-		ApplicationContext ac=new ClassPathXmlApplicationContext(conf);
-		UserDao dao = ac.getBean("userDao",UserDao.class);
-		User user = new User();
-		user.setCn_user_id("123456789");
-		user.setCn_user_name("张三");
-		user.setCn_user_password("123456");
-		user.setCn_user_nick("三儿");
-		dao.save(user);
 		System.out.println(user);
 	}
 }
--- src/test/java/test/service/TestUserService.java
+++ src/test/java/test/service/TestUserService.java
@@ -1,17 +1,13 @@
 package test.service;
 
-import javax.annotation.Resource;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.lcz.cloud_note.dao.UserDao;
-import com.lcz.cloud_note.entity.User;
-import com.lcz.cloud_note.service.UserService;
-import com.lcz.cloud_note.service.UserServiceImpl;
-import com.lcz.cloud_note.util.NoteResult;
+import com.dingjin.ding_note.entity.User;
+import com.dingjin.ding_note.service.UserService;
+import com.dingjin.ding_note.util.NoteResult;
 
 public class TestUserService {
 	private UserService service;
@@ -21,35 +17,10 @@ public class TestUserService {
 		ApplicationContext ac=new ClassPathXmlApplicationContext(conf);
 		service= ac.getBean("userService",UserService.class);
 	}
-	@Test //用例-1:测试用户名不存在的情况
+	@Test //用例-1:测试用户名存在的情况
 	public void test1() {
-		NoteResult<User> result = service.checkLogin("lcz", "123");
-		System.out.println(service.getClass().getName());
-//		System.out.println(result.getStatus());
-//		System.out.println(result.getMsg());
-//		System.out.println(result.getData());
-	}
-	@Test //用例-2:测试密码不存在的情况
-	public void test2() {
-		NoteResult<User> result = service.checkLogin("demo", "123");
-		System.out.println(result.getStatus());
-		System.out.println(result.getMsg());
-		System.out.println(result.getData());
-	}
-	@Test //用例-3:测试登录成功的情况
-	public void test3() {
-		NoteResult<User> result = service.checkLogin("demo", "123456");
-		System.out.println(result.getStatus());
-		System.out.println(result.getMsg());
-		System.out.println(result.getData());
-	}
-	@Test //用例4-预期结果：注册成功（测试注册对象）
-	public void test4() {
-		String name="小二";
-		String password="123456";
-		String nick="二儿";
-		NoteResult<Object> result=service.addUser(name, password, nick);
-		System.out.println(result.getStatus());
+		NoteResult<User> result = service.checkLogin("demo1", "123456");
+//		System.out.println(service.getClass().getName());
 		System.out.println(result.getMsg());
 	}
 }
